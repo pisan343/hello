049a02942255c37dd5adfc9bc7a94eb56a1c798d
Yusuf Pisan <pisan@uw.edu>
2019-01-02 06:16:32 -0800

    additional comments in main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 /**
  * Hello World
+ *
+ * @author Yusuf Pisan
+ * @date 2 Jan 2018
  */
 
 #include <iostream>
 
 using namespace std;
 
+// main program
 int main() {
   // unused variable warning from compiler
+  // CMakeLists.txt must have
+  // set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -Wall -Wextra -Wno-sign-compare")
   int i;
+
   // memory leak warning from cppcheck
   int *leak = new int[10];
+
   // long line warning from cpplint
   cout << "Long lines are bad, limit it to 80 characters, use CLion > Settings > Editor > Code Style > Visual Guides" << endl;
+
   cout << "Hello, World!" << endl;
+
   return 0;
 }
