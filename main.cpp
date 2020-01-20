8fcb607288e501d2f786a0f11cc1326f16b815dc
Yusuf Pisan <pisan@uw.edu>
2020-01-20 15:32:05 -0800

    hello passes tests, upadte simplecompile commenting

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  * Hello World
  *
  * @author Yusuf Pisan
- * @date 2 Jan 2018
+ * @date 20 Jan 2020
  */
 
 #include <iostream>
@@ -12,15 +12,20 @@ using namespace std;
 // main program
 int main() {
   // unused variable warning from compiler
-  // CMakeLists.txt must have
-  // set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -Wall -Wextra -Wno-sign-compare")
-  int i;
+  // For CLion, change CMakeLists.txt to have
+  // set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -Wall
+  // -Wextra -Wno-sign-compare")
 
-  // memory leak
-  int *leak = new int[10];
+  // Uncomment next line to get unused variable warning
+  // int i;
 
-  // long line
-  cout << "Use short lines, max 80 characters. Long lines are bad. When you have long lines, the reader has to scroll left-and-right when reading your code" << endl;
+  // Uncomment next line to get memory leak
+  // int *leak = new int[10];
+
+  // Uncomment AND modify next line to get long line warning
+  // cout << "Use short lines, max 80 characters. Long lines are bad.
+  // When you have long lines, the reader has to scroll left-and-right
+  // when reading your code" << endl;
 
   cout << "Hello, World!" << endl;
 
